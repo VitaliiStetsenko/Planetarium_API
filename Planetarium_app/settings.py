8fc6ab087159992473b3815dcf4055635128daf8
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2026-09-01 18:44:02 +0200

    removed SECRET_KEY and DEBUG to the .env file

--- Planetarium_app/settings.py
+++ Planetarium_app/settings.py
@@ -22,10 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0upp=jq(w2mk12j&020rkct^j4^bs#kc-3m9hfdoz&9bc4s=f9"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
